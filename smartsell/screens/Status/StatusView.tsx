68e5621fcce4d4b97fa4f96715a1c971d2df9a1e
kirankumar.tk <kirankumar.tk@ivymobility.com>
2024-08-16 16:53:15 +0530

    Sqlite DB added

--- smartsell/screens/Status/StatusView.tsx
+++ smartsell/screens/Status/StatusView.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Text } from 'react-native';
+import AppDatabase from '../../Commons/DataBaseHelper';
 const StatusView = () => {
+    useEffect(()=>{
+        AppDatabase.executeFetch("Select AccountId,AccountName from AccountMaster LIMIT 1").then(result => {
+            console.log(result[0].AccountId + '     ' + result[0].AccountName);
+        }).catch(erroe => {
+            console.log(erroe);
+        })
+    },[])
     return (
         <React.Fragment>
             <Text>Status</Text>
